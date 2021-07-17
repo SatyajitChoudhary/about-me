b5dffc6f96d5c345265d962171ea078a22043d0c
Satyajit Choudhary <satyajitchoudhary09@gmail.com>
2021-07-17 21:55:12 +0530

    ResumeUpdated

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,88 +1,79 @@
 export const downloadResumeConfig = {
-  fileName: "cv.pdf",
-  fileLink:
-    "https://drive.google.com/uc?export=download&id=1RpSsgI4GhX2JfsXU0ZIWJSsJoBalSPsc",
+    fileName: "cv.pdf",
+    fileLink: "https://drive.google.com/uc?export=download&id=1Yd55vrQHYAhgG0edXwrmNJx_jXWdjzAl",
 };
 
-export const mySkills = [
-  {
-    title: "React.js",
-    src: "ReactLogo",
-    description:
-      "Front-end library for building web user interfaces.",
-  },
-  {
-    title: "React Native",
-    src: "ReactLogo",
-    description:
-      "Native Mobile framework for Android, iOS.",
-  },
-  {
-    title: "Node.js",
-    src: "NodeLogo",
-    description:
-      "Runtime JS environment for building network apps.",
-  },
-  {
-    title: "Java",
-    src: "JavaIcon",
-    description: "Powerful general-purpose programming language.",
-  },
-  {
-    title: "Redux",
-    src: "ReduxLogo",
-    description:
-      "Predictable state container for JavaScript apps.",
-  },
-  {
-    title: "Linux",
-    src: "LinuxLogo",
-    description:
-      "Operating system connecting computer hardware & software.",
-  },
-  { title: "HTML", src: "HtmlLogo", description: "Backbone of webpages." },
-  { title: "CSS", src: "CssLogo", description: "Paint brush of webpages." },
-  {
-    title: "Javascript",
-    src: "JsLogo",
-    description: "Scripting language that enables interaction in webpages.",
-  },
-  {
-    title: "Material UI",
-    src: "MaterialUILogo",
-    description:
-      "Customizable component library to build UI efficiently.",
-  },
-  {
-    title: "Swagger",
-    src: "SwaggerLogo",
-    description:
-      "Interface description language for describing RESTful APIs.",
-  },
+export const mySkills = [{
+        title: "React.js",
+        src: "ReactLogo",
+        description: "Front-end library for building web user interfaces.",
+    },
+    {
+        title: "React Native",
+        src: "ReactLogo",
+        description: "Native Mobile framework for Android, iOS.",
+    },
+    {
+        title: "Node.js",
+        src: "NodeLogo",
+        description: "Runtime JS environment for building network apps.",
+    },
+    {
+        title: "Java",
+        src: "JavaIcon",
+        description: "Powerful general-purpose programming language.",
+    },
+    {
+        title: "Redux",
+        src: "ReduxLogo",
+        description: "Predictable state container for JavaScript apps.",
+    },
+    {
+        title: "Linux",
+        src: "LinuxLogo",
+        description: "Operating system connecting computer hardware & software.",
+    },
+    { title: "HTML", src: "HtmlLogo", description: "Backbone of webpages." },
+    { title: "CSS", src: "CssLogo", description: "Paint brush of webpages." },
+    {
+        title: "Javascript",
+        src: "JsLogo",
+        description: "Scripting language that enables interaction in webpages.",
+    },
+    {
+        title: "Material UI",
+        src: "MaterialUILogo",
+        description: "Customizable component library to build UI efficiently.",
+    },
+    {
+        title: "Swagger",
+        src: "SwaggerLogo",
+        description: "Interface description language for describing RESTful APIs.",
+    },
 ];
 
 export const myRoutes = [
-  // { title: "Home", click: "#" },
-  { title: "Journey", click: "#journey" },
-  { title: "Skills ", click: "#skills" },
-  { title: "Projects ", click: "#projects" },
-  { title: "Contact", click: "#ContactMe" },
-  { title: "Download Resume ", click: "#resume" },
+    // { title: "Home", click: "#" },
+    { title: "Journey", click: "#journey" },
+    { title: "Skills ", click: "#skills" },
+    { title: "Projects ", click: "#projects" },
+    { title: "Contact", click: "#ContactMe" },
+    { title: "Download Resume ", click: "#resume" },
 ];
 
 export const myPersonalInfo = [
-  { title: "Email", value: "satyajitchoudhary09@gmail.com",link: "mailto:satyajitchoudhary09@gmail.com" },
-  { title: "Call", value: "+91 8981526351",link:"tel:+918981526351" },
-  { title: "Location", value: "Kolkata, West Bengal, India" , link:"#locationMap" },
+    { title: "Email", value: "satyajitchoudhary09@gmail.com", link: "mailto:satyajitchoudhary09@gmail.com" },
+    { title: "Call", value: "+91 8981526351", link: "tel:+918981526351" },
+    { title: "Location", value: "Kolkata, West Bengal, India", link: "#locationMap" },
 ];
 
 export const mapURI =
-  "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14729.254649664099!2d88.36685372539577!3d22.642091066330625!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39f89db1b59942e9%3A0xf47e70d981ac209!2sBaranagar%2C%20West%20Bengal!5e0!3m2!1sen!2sin!4v1623745525417!5m2!1sen!2sin";
+    "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14729.254649664099!2d88.36685372539577!3d22.642091066330625!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39f89db1b59942e9%3A0xf47e70d981ac209!2sBaranagar%2C%20West%20Bengal!5e0!3m2!1sen!2sin!4v1623745525417!5m2!1sen!2sin";
 
 export const socialMediaLinks = {
-  instagram: "#",
-  facebook: "https://www.facebook.com/satyajit.chaudhary.92/",
-  linkedIn: "https://www.linkedin.com/in/satyajitchoudhary/",
-  github: "https://github.com/SatyajitChoudhary",
-  whatsapp: "https://api.whatsapp.com/send?phone=918981526351&text=Hi%20Satyajit%20!%20Interested%20in%20your%20portfolio%20website."
+    instagram: "#",
+    facebook: "https://www.facebook.com/satyajit.chaudhary.92/",
+    linkedIn: "https://www.linkedin.com/in/satyajitchoudhary/",
+    github: "https://github.com/SatyajitChoudhary",
+    whatsapp: "https://api.whatsapp.com/send?phone=918981526351&text=Hi%20Satyajit%20!%20Interested%20in%20your%20portfolio%20website."
 }
